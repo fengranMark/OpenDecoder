19fa1b395e39e4bd01a09619867726913c817fa8
FungJin Mok <464765199@qq.com>
2025-09-09 12:20:11 -0700

    Create iniModel.sh

---
+++ utils/iniModel.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+experiment_name=Qwen2.5-3B-Instruct
+model_path=../checkpoints/Qwen2.5-3B-Instruct
+config_path=../src/model/qwen_decoder/final_config.json
+model_pattern=qwen_decoder
+savedir_imodel=../baseModels/${experiment_name}
+src_path=./src
+savepath_basemodel_namemodules=./basemodel.txt
+savepath_imodel_namemodules=./imodel.txt
+
+log_folder="./logs/${experiment_name}"
+mkdir -p $log_folder
+log_name=$(date +"%m-%d_%H-%M").log
+
+python utils/iniModel.py \
+    --savepath_basemodel_namemodules ${savepath_basemodel_namemodules} \
+    --savepath_imodel_namemodules ${savepath_imodel_namemodules} \
+    --model_name_or_path ${model_path} \
+    --config_path ${config_path} \
+    --model_pattern ${model_pattern} \
+    --src_path ${src_path} \
+    --savedir_imodel ${savedir_imodel} 
